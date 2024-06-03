60f050dc19e27bbc303e4dec58c9a12c2ae7eb26
eannoy <fajarrohino14@gmial.com>
2024-06-03 15:29:05 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# learn payment gateway
---
+++ app/libs/product.js
@@ -0,0 +1,14 @@
+export const product = {
+  id: ~~(Math.random() * 100) + 1,
+  image: "/picture.png",
+  name: "AirSense Pants (Wool Like) ｜Masterpiece | UNIQLO US",
+  price: 350000,
+  description:
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa accusantium, aspernatur provident beatae corporis veniam atque facilis, consequuntur assumenda, vitae dignissimos iste exercitationem dolor eveniet alias eos ullam nesciunt voluptatum",
+  colors: [
+    { value: "putih", label: "Putih" },
+    { value: "biru", label: "Biru" },
+    { value: "coklat", label: "Coklat" },
+    { value: "kuning", label: "Kuning" },
+  ],
+};
